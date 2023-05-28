faf5e784696b8a697d7c2e0e5ec9ccff0f3e6c55
vitor-galdino <httpvittor@gmail.com>
2023-05-28 13:49:43 -0300

    refactor: remove unused labels

--- src/components/DropdownTools.tsx
+++ src/components/DropdownTools.tsx
@@ -1,17 +1,15 @@
 'use client';
-import { PersonResponse } from '@/interfaces';
-import { instance } from '@/services/api';
-import { Download, Edit2, MoreVertical, Trash, User } from 'lucide-react';
+import { UserResponse } from '@/interfaces';
+import { Edit2, MoreVertical, Trash } from 'lucide-react';
 import { useEffect, useRef, useState } from 'react';
 
 interface DropdownToolsProps {
-  person: PersonResponse;
+  user: UserResponse;
   open: number;
   setOpen: (value: number) => void;
-  type: 'customers' | 'contacts';
 }
 
-export default function DropdownTools({ person, open, setOpen, type }: DropdownToolsProps) {
+export default function DropdownTools({ user, open, setOpen }: DropdownToolsProps) {
   const dropdownRef = useRef<HTMLDivElement>(null);
   const [dropdownPosition, setDropdownPosition] = useState<{ top: number; left: number; }>({ top: 0, left: 0 });
 
@@ -50,31 +48,23 @@ export default function DropdownTools({ person, open, setOpen, type }: DropdownT
       <button
         onClick={(e) => {
           handleButtonClick(e);
-          setOpen(open == person.id ? 0 : person.id);
+          setOpen(open == user.id ? 0 : user.id);
         }}
         className={
           `flex items-center justify-center p-2 rounded-full w-9 h-9 hover:bg-gray-200/40 hover:text-blue-500 
-           ${open == person.id ? 'text-blue-500 bg-gray-200/40' : 'text-gray-700'}`
+           ${open == user.id ? 'text-blue-500 bg-gray-200/40' : 'text-gray-700'}`
         }
         id='dropdownButton'
       >
         <MoreVertical id='dropdownButton' />
       </button>
-      {open == person.id && (
+      {open == user.id && (
         <div
           onClick={() => setOpen(0)}
           className='fixed z-40 block py-1 mt-12 -mr-1 border border-gray-100 rounded-lg shadow-lg w-44 backdrop-blur-sm'
           ref={dropdownRef}
           style={{ top: dropdownPosition.top, left: dropdownPosition.left }}
         >
-          {type == 'customers' && (
-            <label className='relative flex items-center justify-start px-4 py-2 text-gray-700 cursor-pointer decoration-clone hover:bg-gray-100/60'>
-              <div className='flex items-center gap-3 select-none'>
-                <User strokeWidth={1.5} className='w-5 h-5 text-gray-700/90' />
-                Ver Contatos
-              </div>
-              <div className='absolute bottom-0 left-0 right-0 w-11/12 h-px mx-auto bg-gray-300 rounded-full'></div>
-            </label>)}
           <label className='relative flex items-center justify-start px-4 py-2 text-gray-700 cursor-pointer decoration-clone hover:bg-gray-100/60'>
             <div className='flex items-center gap-3 select-none'>
               <Edit2 strokeWidth={1.5} className='w-5 h-5 text-gray-700/90' />
@@ -82,17 +72,6 @@ export default function DropdownTools({ person, open, setOpen, type }: DropdownT
             </div>
             <div className='absolute bottom-0 left-0 right-0 w-11/12 h-px mx-auto bg-gray-300 rounded-full'></div>
           </label>
-          {type == 'customers' && (
-            <a href={`${instance.defaults.baseURL}/reports/${person.id}`}
-              download
-              className='relative flex items-center justify-start px-4 py-2 text-gray-700 cursor-pointer hover:bg-gray-100/60'
-            >
-              <div className='flex items-center gap-3 select-none'>
-                <Download strokeWidth={1.5} className='w-5 h-5 text-gray-700/90' />
-                Relatório
-              </div>
-              <div className='absolute bottom-0 left-0 right-0 w-11/12 h-px mx-auto bg-gray-300 rounded-full'></div>
-            </a>)}
           <label className='relative flex items-center justify-start px-4 py-2 cursor-pointer hover:bg-red-100/40 text-red-500/90'>
             <div className='flex items-center gap-3 select-none'>
               <Trash strokeWidth={1.5} className='w-5 h-5 text-red-500/80' />
