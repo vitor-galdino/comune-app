77de0e3ae9ffc4ec0c2a3b08ec1373789b846be5
vitor-galdino <httpvittor@gmail.com>
2023-05-28 19:40:30 -0300

    feat: add function to handle download

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,6 +1,8 @@
 'use client';
 import { ContactResponse } from '@/interfaces';
-import { Plus, Search } from 'lucide-react';
+import { instance } from '@/services/api';
+import { saveAs } from 'file-saver';
+import { Download, Plus, Search } from 'lucide-react';
 import { useState } from 'react';
 import DropdownTools from './DropdownTools';
 
@@ -11,6 +13,25 @@ interface TableProps {
 export default function Table({ response }: TableProps) {
   const [searchQuery, setSearchQuery] = useState<string>('');
   const [open, setOpen] = useState<number>(0);
+
+  async function handleDownloadPDF() {
+    try {
+      const res = await instance.get('/reports', {
+        responseType: 'blob'
+      });
+
+      const date = new Date();
+      const formattedDateTime = date.toLocaleString('pt-BR')
+        .replaceAll('/', '-')
+        .split(',')[0];
+      const fileName = `relatorio_${formattedDateTime}`;
+      saveAs(res.data, fileName);
+
+    } catch (err) {
+      console.error(err);
+      throw new Error('Error');
+    }
+  }
 
   const headings = [
     { key: 'fullName', value: 'Nome' },
@@ -27,7 +48,7 @@ export default function Table({ response }: TableProps) {
         <div className='flex-1 pr-4'>
           <div className='relative md:w-1/3'>
             <input
-              type='search'
+              type='text'
               className='w-full py-2 pl-10 pr-4 font-medium text-gray-600 rounded-lg shadow focus:outline-none'
               placeholder='Search...'
               value={searchQuery}
@@ -39,10 +60,18 @@ export default function Table({ response }: TableProps) {
           </div>
         </div>
         <div>
-          <div className='flex rounded-lg shadow'>
+          <div className='flex gap-3 rounded-lg'>
+            <div>
+              <button onClick={handleDownloadPDF}
+                className='flex items-center gap-2 px-2 py-2 font-semibold text-gray-500 bg-white rounded-lg shadow hover:text-blue-500 md:px-4'
+              >
+                <Download className='w-5 h-5' strokeWidth={2.5} />
+                <span className='hidden md:block'>Relatório</span>
+              </button>
+            </div>
             <div>
               <button
-                className='flex items-center gap-1 px-2 py-2 font-semibold text-gray-500 bg-white rounded-lg hover:text-blue-500 md:px-4'
+                className='flex items-center gap-1 px-2 py-2 font-semibold text-gray-500 bg-white rounded-lg shadow hover:text-blue-500 md:px-4'
               >
                 <Plus className='w-5 h-5' strokeWidth={2.5} />
                 <span className='hidden md:block'>Adicionar Contato</span>
