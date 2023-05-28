b1facd7b448da61b229f8f25df682f85819f789f
vitor-galdino <httpvittor@gmail.com>
2023-05-28 14:40:12 -0300

    feat: add contacts interfaces and add password key in User interface

--- src/interfaces/index.tsx
+++ src/interfaces/index.tsx
@@ -1,10 +1,26 @@
-export interface Person {
+export interface User {
+  fullName: string;
+  email: string;
+  password: string;
+  phone: string;
+}
+
+export interface UserResponse extends Omit<User, 'password'> {
+  id: number;
+  createdAt: string;
+}
+
+export interface Contact {
   fullName: string;
   email: string;
   phone: string;
 }
 
-export interface PersonResponse extends Person {
+export interface ContactResponse extends Contact {
   id: number;
   createdAt: string;
+}
+
+export interface UserAndContactsResponse extends UserResponse {
+  contacts: ContactResponse[];
 }
