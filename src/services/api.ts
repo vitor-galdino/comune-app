f8903784d63dabd7760d5eefdd34da33473ebcaa
vitor-galdino <httpvittor@gmail.com>
2023-05-31 19:57:58 -0300

    feat: add function to handle with user token

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,5 +2,13 @@ import axios from 'axios';
 
 export const instance = axios.create({
   baseURL: 'http://localhost:3000',
-  timeout: 15000
+  timeout: 15000,
 });
+
+export function setAuthToken(token: string) {
+  if (token) {
+    instance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  } else {
+    delete instance.defaults.headers.common['Authorization'];
+  }
+}
