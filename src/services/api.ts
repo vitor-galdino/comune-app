2413f57e3f50f9d8f5680e0812b289b897b52e1e
vitor-galdino <httpvittor@gmail.com>
2023-06-01 00:14:26 -0300

    feat: add Cookies manipulation and validation

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
 export const instance = axios.create({
   baseURL: 'http://localhost:3000',
@@ -8,7 +9,14 @@ export const instance = axios.create({
 export function setAuthToken(token: string) {
   if (token) {
     instance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    Cookies.set('token', token, { secure: true });
   } else {
     delete instance.defaults.headers.common['Authorization'];
+    Cookies.remove('token');
   }
+}
+
+const token = Cookies.get('token');
+if (token) {
+  setAuthToken(token);
 }
