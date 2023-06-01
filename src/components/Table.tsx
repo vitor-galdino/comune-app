c99d1d762eea4e8f0d086d545e4cd162e26a5edc
vitor-galdino <httpvittor@gmail.com>
2023-06-01 19:44:30 -0300

    refactor: replace default blue color to branding color

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -22,14 +22,14 @@ export default function Table() {
       <h1 className='py-4 mb-10 text-3xl font-bold text-gray-800 border-b'>Contatos</h1>
       <div className='flex items-center justify-between mb-4'>
         <div className='flex-1 pr-4'>
-          <div className='relative md:w-1/3'>
+          <div className='relative md:w-1/3 group'>
             <input
               type='text'
-              className='w-full py-2 pl-10 pr-4 font-medium text-gray-600 rounded-lg shadow focus:outline-none'
-              placeholder='Search...'
+              className='w-full py-2 pl-10 pr-4 border-b border-transparent rounded-lg shadow focus:outline-none focus:border-b focus:border-branding-blue/60 hover:border-branding-blue/60'
+              placeholder='Pesquisar contato...'
               onChange={(e: ChangeEvent<HTMLInputElement>) => filterContactNameOnInput(e.target.value)}
             />
-            <div className='absolute top-0 bottom-0 left-0 inline-flex items-center p-2 text-gray-400/60'>
+            <div className='absolute top-0 bottom-0 left-0 inline-flex items-center p-2 text-gray-400/60 group-focus-within:text-branding-blue'>
               <Search className='w-5 h-5 ' strokeWidth={2.5} />
             </div>
           </div>
@@ -38,7 +38,7 @@ export default function Table() {
           <div className='flex gap-3 rounded-lg'>
             <div>
               <button onClick={handleDownloadPDF}
-                className='flex items-center gap-2 px-2 py-2 font-semibold text-gray-500 bg-white rounded-lg shadow hover:text-blue-500 md:px-4'
+                className='flex items-center gap-2 px-2 py-2 font-semibold text-gray-500 bg-white rounded-lg shadow hover:text-branding-blue md:px-4'
               >
                 <Download className='w-5 h-5' strokeWidth={2.5} />
                 <span className='hidden md:block'>Relatório</span>
@@ -46,7 +46,7 @@ export default function Table() {
             </div>
             <div>
               <button
-                className='flex items-center gap-1 px-2 py-2 font-semibold text-gray-500 bg-white rounded-lg shadow hover:text-blue-500 md:px-4'
+                className='flex items-center gap-1 px-2 py-2 font-semibold text-gray-500 bg-white rounded-lg shadow hover:text-branding-blue md:px-4'
               >
                 <Plus className='w-5 h-5' strokeWidth={2.5} />
                 <span className='hidden md:block'>Adicionar Contato</span>
