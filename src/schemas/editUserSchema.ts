bb7ae4530872b3dbba98b77cd55ebaeb4aabbda7
vitor-galdino <httpvittor@gmail.com>
2023-06-04 17:27:36 -0300

    feat: add editUserSchema

---
+++ src/schemas/editUserSchema.ts
@@ -0,0 +1,41 @@
+import { z } from 'zod';
+
+export const editUserSchema = z.object({
+  fullName: z.string()
+    .max(120, 'A senha ultrapassou o limite de 120 caracteres')
+    .optional()
+    .refine(
+      (value) => value === undefined || value === '' || value.length >= 3,
+      'O nome precisa ter no mínimo 3 caracteres'
+    ),
+  email: z.string()
+    .max(45, 'O e-mail ultrapassou o limite de 45 caracteres')
+    .optional()
+    .refine(
+      (value) =>
+        value === undefined ||
+        value === '' ||
+        z.string().email().safeParse(value).success,
+      'Formato de e-mail inválido'
+    ),
+  password: z.string()
+    .max(120, 'A senha ultrapassou o limite de 120 caracteres')
+    .optional()
+    .refine(
+      (value) => value === undefined || value === '' || value.length >= 3,
+      'O nome precisa ter no mínimo 3 caracteres'
+    ),
+  phone: z.string()
+    .max(14, 'O número ultrapassou o limite de 14 caracteres')
+    .optional(),
+}).transform((obj) => {
+  const newObj: { [key: string]: string | undefined; } = { ...obj };
+  Object.keys(newObj).forEach((key) => {
+    if (newObj[key] === '') {
+      delete newObj[key];
+    }
+  });
+  if (Object.keys(newObj).length) {
+    return newObj;
+  }
+});
