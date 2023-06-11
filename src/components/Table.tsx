938960b748d8fd4e5dec6aa34eab06922d6b47ea
vitor-galdino <httpvittor@gmail.com>
2023-06-11 20:29:59 -0300

    fix: change index position

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -111,7 +111,7 @@ export default function Table() {
                 {headings.map((heading) => (
                   <th
                     key={heading.key}
-                    className={`bg-gray-200 sticky top-0 z-50 border-b border-gray-200  px-6 py-4 text-gray-500 font-bold tracking-wider uppercase text-xs`}
+                    className={`bg-gray-200 sticky top-0 z-10 border-b border-gray-200  px-6 py-4 text-gray-500 font-bold tracking-wider uppercase text-xs`}
                   >
                     {heading.value}
                   </th>
