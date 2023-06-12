aa37065b2a4b8d36b78a8968b77bb65c805b57a8
vitor-galdino <httpvittor@gmail.com>
2023-06-11 21:05:37 -0300

    feat: add new background color and padding top

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import Table from '@/components/Table';
 
 export default function Dashboard() {
   return (
-    <section className='bg-slate-100 h-screen'>
+    <section className='bg-white h-screen pt-28'>
       <Table />
     </section>
   );
